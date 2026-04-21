415c802d6d7ac1b0fb8a904c7e1e6f25b3ac1324
Ahmed sultan <ahmedsultan1960@gmail.com>
2026-04-21 14:06:53 +0200

    moved all the bash scripts into the bash file.

--- scripting_jurney/admin.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-USER_ID=$(id -u)
-
-if [[ "$USER_ID" -ne 0 ]]; then 
-	echo "Error: this script must be run as root."
-	exit 1
-fi
-echo "Welcom adminstrator"
--- scripting_jurney/string_comparison.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-read  -p " Enter your name: " user_name
-if [[ -z "$user_name" ]]; then 
-	echo "Error : Name can not be empty. Exiting."
-	exit 1
-fi
-echo "Welcome, $user_anme!"
